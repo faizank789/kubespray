1d0dd23bbc8c1f0b2599ed63dcf25cdeda186882
faizank789 <faizank789@gmail.com>
2023-02-21 18:37:22 +0530

    Update setup-centos.sh

--- setup-centos.sh
+++ setup-centos.sh
@@ -13,3 +13,16 @@ python3 -m pip install ansible \
 pip3 install --user netaddr \
 
 pip3 install ruamel.yaml \
+
+pip3 install jmespath 
+
+###
+
+ # install to master && client machine
+  
+
+yum install python3-pip git -y 
+
+pip3 install --upgrade pip \
+
+pip3 install jmespath 
